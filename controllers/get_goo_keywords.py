e11283a31cd82555e554c58140f70ef63474dc5f
watarun <watarun54@gmail.com>
2019-08-13 23:02:24 +0700

    dev-23: fix google domain check logic

--- controllers/get_goo_keywords.py
+++ controllers/get_goo_keywords.py
@@ -2,7 +2,7 @@ from urllib.parse import urlparse, parse_qs
 
 def get_keywords(url):
     parsed_url = urlparse(url)
-    if "google.com" not in parsed_url.netloc: return None
+    if "www.google" not in parsed_url.netloc: return None
     qs = urlparse(url).query
     qs_d = parse_qs(qs)
     return qs_d['q'][0] + " | Google Search"
