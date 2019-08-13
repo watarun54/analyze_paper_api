bb358842438313caeabb58fe31ffa43411d8e5f8
watarun <watarun54@gmail.com>
2019-08-13 21:58:23 +0700

    dev-23: fix get_keywords in get_goo_keywords

--- controllers/get_goo_keywords.py
+++ controllers/get_goo_keywords.py
@@ -2,7 +2,7 @@ from urllib.parse import urlparse, parse_qs
 
 def get_keywords(url):
     parsed_url = urlparse(url)
-    if parsed_url.netloc != "www.google.com": return None
+    if "google.com" not in parsed_url.netloc: return None
     qs = urlparse(url).query
     qs_d = parse_qs(qs)
     return qs_d['q'][0]
